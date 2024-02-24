ab7e1e4800bd8f168ec6458474f5de81597479d8
VagnerNatvidade <vagnerflorindojunior@gmail.com>
2024-02-24 20:49:17 -0300

    tratamento de erro(teste)

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,14 @@
+// import AppError
+const AppError = require("../utils/AppError");
+
 // processing of user request
 class UsersController {
   create(req, res) {
     const { name, email, password } = req.body;
+
+    if (!name) {
+      throw new AppError("Name is required");
+    }
 
     res.json({ name, email, password });
   }

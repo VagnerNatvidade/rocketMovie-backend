a7d9e53b732f09bb3cb17245dc1935a0d61e995a
VagnerNatvidade <vagnerflorindojunior@gmail.com>
2024-02-23 21:52:18 -0300

    inicia o server

---
+++ src/server.js
@@ -0,0 +1,9 @@
+// import express
+const express = require("express");
+
+// start express
+const app = express();
+
+// set a port
+const PORT = 3333;
+app.listen(PORT, () => console.log("server is running"));

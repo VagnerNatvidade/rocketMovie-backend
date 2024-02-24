890ab56b540dd7dc8003c36984deb3b23511f427
VagnerNatvidade <vagnerflorindojunior@gmail.com>
2024-02-23 22:02:34 -0300

    testa o servidor

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const express = require("express");
 // start express
 const app = express();
 
+// send a page message (text)
+app.get("/message", (req, res) => {
+  res.send("Hello World");
+});
 // set a port
 const PORT = 3333;
 app.listen(PORT, () => console.log("server is running"));

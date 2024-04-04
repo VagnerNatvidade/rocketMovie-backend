3cf46daaf7af939b2a543e71145d23702116a9b2
VagnerNatvidade <vagnerflorindojunior@gmail.com>
2024-04-04 13:56:01 -0300

    aplica o middleware nas rotas - com error

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -8,7 +8,7 @@ class UsersController {
     const { name, email, password } = req.body;
 
     const database = await sqliteConnection();
-    
+
     const checkUserExists = await database.get(
       "SELECT * FROM users WHERE email = (?)",
       [email]
@@ -30,11 +30,13 @@ class UsersController {
 
   async update(req, res) {
     const { name, email, password, old_password } = req.body;
-    const { id } = req.params;
+    const user_id = req.user.id;
 
     const database = await sqliteConnection();
 
-    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [
+      user_id,
+    ]);
 
     if (!user) {
       throw new AppError("User not exist");
@@ -74,7 +76,7 @@ class UsersController {
     password = ?,
     updated_at = DATETIME('now')
     WHERE id = ?`,
-      [user.name, user.email, user.password, id]
+      [user.name, user.email, user.password, user_id]
     );
 
     return res.json();
